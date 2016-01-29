7c3d852ddb95af95daf801c9bc742042dbf76fa6
Maximilian Kalus <info@auxnet.de>
2016-01-29 12:05:43 +0100

    first commit

---
+++ Classes/ViewHelpers/CategoriesViewHelper.php
@@ -0,0 +1,102 @@
+<?php
+namespace AUXNET\MakDataviewhelpers\ViewHelpers;
+
+    /***************************************************************
+     *  Copyright notice
+     *
+     *  (c) 2016 Dr. Maximilian Kalus <info@auxnet.de>, AUXNET
+     *
+     *  All rights reserved
+     *
+     *  This script is part of the TYPO3 project. The TYPO3 project is
+     *  free software; you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation; either version 3 of the License, or
+     *  (at your option) any later version.
+     *
+     *  The GNU General Public License can be found at
+     *  http://www.gnu.org/copyleft/gpl.html.
+     *
+     *  This script is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  This copyright notice MUST APPEAR in all copies of the script!
+     ***************************************************************/
+
+/**
+ * Category view helper - return categories from input
+ *
+ * @package mak_dataviewhelpers
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class CategoriesViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+    /**
+     * @var \TYPO3\CMS\Extbase\Domain\Repository\CategoryRepository
+     */
+    protected $categoryRepository;
+
+    /**
+     * Inject repository to enable DI
+     *
+     * @param \TYPO3\CMS\Extbase\Domain\Repository\CategoryRepository $categoryRepository
+     * @return void
+     */
+    public function injectCategoryRepository(\TYPO3\CMS\Extbase\Domain\Repository\CategoryRepository $categoryRepository) {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @param string|array $categories categories either string (comma separated) or list of uids as array
+     * @param string $as render as variable instead of returning array directly
+     * @param boolean $firstOnly if true return only first record
+     * @param boolean $titleOnly if true return titles only (either array or string if firstOnly set)
+     * @return array|string|\TYPO3\CMS\Extbase\Domain\Model\Category array of categories (if as is set) or output or single entry
+     */
+    public function render($categories, $as = NULL, $firstOnly = FALSE, $titleOnly = FALSE) {
+        // explode if string
+        if (is_string($categories)) $categories = explode(',', $categories);
+
+        // get categories
+        $query = $this->categoryRepository->createQuery();
+        $query->matching($query->in('uid', $categories));
+
+        $categoriesFound = $query->execute();
+
+        // nothing found?
+        if ($categoriesFound->count() == 0)
+            return NULL;
+
+        if ($firstOnly) {
+            $categoriesFound = $categoriesFound->getFirst();
+
+            // title only?
+            /**
+             * @var \TYPO3\CMS\Extbase\Domain\Model\Category $categoriesFound
+             */
+            if ($titleOnly) $categoriesFound = $categoriesFound->getTitle();
+        }
+        // title only? -> create array of titles
+        elseif ($titleOnly) {
+            $titles = array();
+            /**
+             * @var \TYPO3\CMS\Extbase\Domain\Model\Category $category
+             */
+            foreach ($categoriesFound as $category)
+                $titles[] = $category->getTitle();
+
+            $categoriesFound = $titles;
+        }
+
+        // return directly
+        if (empty($as)) return $categoriesFound;
+
+        $this->templateVariableContainer->add($as, $categoriesFound);
+        $output = $this->renderChildren();
+        $this->templateVariableContainer->remove($as);
+        return $output;
+    }
+}
---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "mak_dataviewhelper"
+ *
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Data View Helpers',
+	'description' => 'ViewHelpers to render stuff in FE.',
+	'category' => 'plugin',
+	'author' => 'Dr. Maximilian Kalus',
+	'author_email' => 'info@auxnet.de',
+	'author_company' => 'AUXNET',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'extbase' => '6.0',
+			'fluid' => '6.0',
+			'typo3' => '6.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
