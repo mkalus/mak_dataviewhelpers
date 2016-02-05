6e2b10b0008ca75d2b6e86d6f56a7f096529e7e1
Maximilian Kalus <info@auxnet.de>
2016-02-05 12:40:08 +0100

    Extend support to TYPO3 6.2.x.

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.2',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-7.99.99',
+			'typo3' => '6.2.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
